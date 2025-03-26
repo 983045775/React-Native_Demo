65aba65e19f774982b4d4ff949385293201f98ec
chengliu12 <chengliu12@iflytek.com>
2025-03-26 15:47:59 +0800

    add:增加点击效果查看

--- src/common/BankCardView.js
+++ src/common/BankCardView.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {ImageBackground, Image, Text,ScrollView, View, StyleSheet} from 'react-native';
+import {
+  ImageBackground,
+  Image,
+  Text,
+  ScrollView,
+  View,
+  StyleSheet,
+} from 'react-native';
 import bgCard from '../assets/bg_card.png';
 import iconBank from '../assets/icon_bank.png';
 
@@ -9,15 +16,26 @@ import iconBank from '../assets/icon_bank.png';
  * @constructor
  */
 function BankCardView() {
+
+  const toastConsole =()=>{
+    console.log("点了就点了呗啊");
+  }
   return (
     <View style={styles.root}>
       <ImageBackground style={styles.cardBg} source={bgCard}>
         <Image source={iconBank} style={styles.logo} />
         <View>
-          <Text style={styles.titleName}>招商银行</Text>
+          <Text
+            style={styles.titleName}
+            onPress={(event) => {
+              toastConsole();
+            }}>
+            招商银行
+          </Text>
           <Text style={styles.titleName2}>储蓄卡</Text>
-          <View style={{flexDirection: 'row',marginTop:20,alignItems: 'center'}}>
-            <Text style={styles.titleName3}>●●●●   ●●●●   ●●●●   </Text>
+          <View
+            style={{flexDirection: 'row', marginTop: 20, alignItems: 'center'}}>
+            <Text style={styles.titleName3}>●●●● ●●●● ●●●● </Text>
             <Text style={styles.titleName4}>7475</Text>
           </View>
         </View>
@@ -54,26 +72,26 @@ const styles = StyleSheet.create({
   },
   titleName: {
     color: 'white',
-    marginLeft:8,
-    marginTop:18,
+    marginLeft: 8,
+    marginTop: 18,
     fontSize: 14,
     fontWeight: 'bold',
   },
   titleName2: {
     color: '#ffffffA0',
-    marginLeft:8,
-    marginTop:2,
+    marginLeft: 8,
+    marginTop: 2,
     fontSize: 12,
   },
   titleName3: {
     color: '#ffffff',
-    marginLeft:8,
+    marginLeft: 8,
     fontSize: 8,
   },
   titleName4: {
     color: '#ffffff',
-    marginLeft:8,
-    marginTop:2,
+    marginLeft: 8,
+    marginTop: 2,
     fontSize: 16,
   },
 });
