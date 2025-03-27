d6fc818ecc226cf46dd0334879bcc3a24965dc80
luca.liu <luca.liu@quectel.com>
2025-03-27 21:01:09 +0800

    add:增加ScrollsView练习demo

--- App.tsx
+++ App.tsx
@@ -9,23 +9,24 @@ import React from 'react';
 import BankCardView from './src/common/BankCardView.tsx';
 import InputViewDemo from './src/common/InputViewDemo.tsx';
 import PressableView from './src/common/PressableView.js';
+import ScrollsView from "./src/common/ScrollsView.js";
 import {ScrollView, View} from 'react-native';
 
 function App() {
-
-    return (
-    <ScrollView>
-      <View
-        style={{
-          flexDirection: 'column',
-          width: "100%",
-          height: 8100,
-        }}>
-          <BankCardView />
-          <InputViewDemo />
-          <PressableView />
-      </View>
-    </ScrollView>
+  return (
+    // <ScrollView stickyHeaderIndices={[0]}>
+    //   <View
+    //     style={{
+    //       flexDirection: 'column',
+    //       width: '100%',
+    //       height: 8100,
+    //     }}>
+    //     <BankCardView />
+    //     <InputViewDemo />
+    //     <PressableView />
+        <ScrollsView />
+      // </View>
+    // </ScrollView>
   );
 }
 
---
+++ src/common/ScrollsView.js
@@ -0,0 +1,62 @@
+import React, {use, useRef} from 'react';
+import {ScrollView, StyleSheet, Text} from 'react-native';
+
+function ScrollsView() {
+  const scrollRef = useRef(null);
+  const Views = () => {
+    //定义一个数组
+    const viewArrays = [];
+    for (let i = 0; i < 30; i++) {
+      viewArrays.push(
+        <Text style={styles.txt} key={i}>
+          {'item  = ' + (i + 1)}
+        </Text>,
+      );
+    }
+    //返回数组里面全是text标签
+    return viewArrays;
+  };
+
+  return (
+    <ScrollView
+      ref={scrollRef}
+      stickyHeaderIndices={[0, 1]}
+      style={styles.root}>
+      <Text
+        style={styles.txt}
+        onPress={() => {
+          console.log('press');
+          scrollRef.current.scrollTo({y: 0, animated: true});
+        }}>
+        置顶
+      </Text>
+      <Text
+        style={styles.txt}
+        onPress={() => {
+          console.log('press');
+          scrollRef.current.scrollToEnd()
+        }}>
+        底部
+      </Text>
+      {Views()}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    width: '100%',
+    height: 400,
+    paddingLeft: 20,
+    backgroundColor: '#38383812',
+  },
+  txt: {
+    padding: 40,
+    backgroundColor: '#38383812',
+    marginTop: 20,
+    marginBottom: 20,
+    fontSize: 20,
+  },
+});
+
+export default ScrollsView;
