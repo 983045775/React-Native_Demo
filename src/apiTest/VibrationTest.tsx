5507eb1dec5d1fb3bf5e16243f1df186e77b09c9
luca.liu <luca.liu@quectel.com>
2025-04-17 19:55:05 +0800

    增加Transform练习

--- src/apiTest/VibrationTest.tsx
+++ src/apiTest/VibrationTest.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import {
-    Text,
-    View,
-    Button,
-    TouchableOpacity,
-    StyleSheet,
-    BackHandler,
-    PermissionsAndroid,
-    Vibration, ToastAndroid,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+  BackHandler,
+  PermissionsAndroid,
+  Vibration,
+  ToastAndroid,
+  Image,
 } from 'react-native';
+
+// @ts-ignore
+import avatar from '../assets/avatar.png';
 
 function vibrationTest() {
   const startVibration = () => {
-    Vibration.vibrate([ 100,200], true);
+    Vibration.vibrate([100, 200], true);
   };
 
-  const closeVibration = ()=>{
-      Vibration.cancel()
-  }
+  const closeVibration = () => {
+    Vibration.cancel();
+  };
 
   return (
     <View>
@@ -25,14 +30,35 @@ function vibrationTest() {
         title={'开始振动'}
         onPress={() => {
           startVibration();
-          ToastAndroid.showWithGravity("开始振动",ToastAndroid.SHORT,ToastAndroid.TOP)
+          ToastAndroid.showWithGravity(
+            '开始振动',
+            ToastAndroid.SHORT,
+            ToastAndroid.TOP,
+          );
         }}
       />
-      <Button title={'结束振动'} onPress={()=>{
-          closeVibration()
-      }}/>
+      <Button
+        title={'结束振动'}
+        onPress={() => {
+          closeVibration();
+        }}
+      />
+
+      <Image style={styles.photo}  source={avatar} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    photo:{
+        transform:[
+            // {scaleX:1.2},
+            // {translateX:30},
+            // {rotateX:"100deg"}
+            {rotate:"90deg"}
+        ]
+
+    }
+})
 
 export default vibrationTest;
